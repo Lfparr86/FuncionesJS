65bc63fd29d3e0889f2fc9567139b0744f71878c
lfparr86 <luisFernando.parravaquerizo@gmail.com>
2022-11-03 19:36:05 +0100

    Temporizador

---
+++ 005tempToZero/005tempToZero.js
@@ -0,0 +1,16 @@
+
+let numeroMinutos = + prompt("Dime cuantos minutos");
+
+let numeroMinutosMilisegundos = numeroMinutos * 60000;
+
+let numeroSegundos = + prompt("Dime cuantos segundos");
+
+let numeroSegundosMilisegundos = numeroSegundos *1000;
+
+function temporizador(numeroMinutos,numeroSegundos) {
+
+    setTimeout(() => {
+        
+    }, timeout);
+    
+}

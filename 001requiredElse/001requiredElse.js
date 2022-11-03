a8a2467e48d993b16fcd58db0e5c251b6b87e4c8
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-03 14:02:23 +0100

    Primera subida

---
+++ 001requiredElse/001requiredElse.js
@@ -0,0 +1,20 @@
+/*
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm('¿Tus padres te permitieron?');
+  }
+}
+*/
+
+let age =5;
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      // ...
+      return confirm('¿Tus padres te permitieron?');
+    }
+  }
